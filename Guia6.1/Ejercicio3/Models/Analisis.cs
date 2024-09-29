a8b4fab05bb26f87e395e74d8a2fa8e67d35950b
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2024-09-29 16:38:43 -0300

    agregando uml

---
+++ Guia6.1/Ejercicio3/Models/Analisis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Analisis
+    { 
+        public int CantidadSecuencias { get { return secuenicas.Count; } }
+
+        private List<Secuencia> secuenicas = new List<Secuencia>();
+    }
+}
---
+++ Guia6.1/Ejercicio3/Models/Proteina.cs
@@ -0,0 +1,20 @@
+﻿using Ejercicio2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Proteina
+    {
+        public string Secuencia { get; private set; }
+
+        public Proteina(string secuencia)
+        {
+            Secuencia = secuencia;
+        }
+    }
+}
